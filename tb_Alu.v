b4677bbf5f70b005310e87dfec5c2a9d27f6f635
Thiago Vieira <thiagovieiraas@gmail.com>
2014-03-11 18:33:05 -0300

    Update tb_Alu.v

--- tb_Alu.v
+++ tb_Alu.v
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------------
-// Design Name: Testbench for the Alu module.
+// Design Name: tb_Alu.
 // Function: Test if the output of all operations is what is expected to be.
 //----------------------------------------------------------------------------------
 module tb_Alu;
@@ -16,16 +16,87 @@ module tb_Alu;
 		$dumpfile("result.vcd");
 		$dumpvars(0, tb_Alu);
 
-		//Neste teste overflow tem que ser 0
-		$display("Teste para overflow: Neste teste o sinal de overflow tem que ser 1(um)\n");
+		//Neste teste overflow tem que ser 1(um)
+		$display("Teste para overflow: Neste teste o sinal de overflow tem que ser 1(um)");
 		a = 32'b01111111111111111111111111111111;
 		b = 32'b01111111111111111111111111111111;
         op = 3'b010;
 		unsig = 0;
-		$display("a = %b\nb = %b\nop = %b\n", a, b, op);
+		$display("a = %b\nb = %b\nop = %b", a, b, op);
 		#10;
 		$display("Result: compout=%b  overflow=%b  aluout=%b\n\n", compout, overflow, aluout);
 
-		//$display();
+		//Neste teste, overflow tem que ser 0(zero)
+		$display("Teste para overflow: Neste teste o sinal de overflow tem que ser 0(zero)");
+		a = 32'b01000011011001100111000100000111;
+		b = 32'b00001100110001100100011001111000;
+        op = 3'b010;
+		unsig = 0;
+		$display("a = %b\nb = %b\nop = %b", a, b, op);
+		#10;
+		$display("Result: compout=%b  overflow=%b  aluout=%b\n\n", compout, overflow, aluout);
+		
+		
+		//INICIO TESTE OPERACOES
+		$display("Teste das operações:\n");
+		$display("AND");
+		a = 32'b01000011011001100111000100000111;
+		b = 32'b00001100110001100100011001111000;
+        op = 3'b000;
+		unsig = 0;
+		$display("a = %b\nb = %b\nop = %b", a, b, op);
+		#10;
+		$display("Result: compout=%b  overflow=%b  aluout=%b\n\n", compout, overflow, aluout);
+		
+		
+		$display("OR");
+		a = 32'b01000011011001100111000100000111;
+		b = 32'b00001100110001100100011001111000;
+        op = 3'b001;
+		unsig = 0;
+		$display("a = %b\nb = %b\nop = %b", a, b, op);
+		#10;
+		$display("Result: compout=%b  overflow=%b  aluout=%b\n\n", compout, overflow, aluout);
+	
+
+		$display("ADD");
+		a = 32'b01000011011001100111000100000111;
+		b = 32'b00001100110001100100011001111000;
+        op = 3'b010;
+		unsig = 0;
+		$display("a = %b\nb = %b\nop = %b", a, b, op);
+		#10;
+		$display("Result: compout=%b  overflow=%b  aluout=%b\n\n", compout, overflow, aluout);
+		
+
+		$display("NOR");
+		a = 32'b01000011011001100111000100000111;
+		b = 32'b00001100110001100100011001111000;
+        op = 3'b100;
+		unsig = 0;
+		$display("a = %b\nb = %b\nop = %b", a, b, op);
+		#10;
+		$display("Result: compout=%b  overflow=%b  aluout=%b\n\n", compout, overflow, aluout);
+		
+
+		$display("XOR");
+		a = 32'b01000011011001100111000100000111;
+		b = 32'b00001100110001100100011001111000;
+        op = 3'b101;
+		unsig = 0;
+		$display("a = %b\nb = %b\nop = %b", a, b, op);
+		#10;
+		$display("Result: compout=%b  overflow=%b  aluout=%b\n\n", compout, overflow, aluout);
+		
+		
+		$display("SUB");
+		a = 32'b01000011011001100111000100000111;
+		b = 32'b00001100110001100100011001111000;
+        op = 3'b110;
+		unsig = 0;
+		$display("a = %b\nb = %b\nop = %b", a, b, op);
+		#10;
+		$display("Result: compout=%b  overflow=%b  aluout=%b\n\n", compout, overflow, aluout);
+		//END TESTE OPERACOES
 	end
 endmodule
